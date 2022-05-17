5dc0b68d8a0a970741c620072f6302756e9427b7
Julia6996 <ronin1991@rambler.ru>
2022-05-17 12:41:07 +0300

    changes

---
+++ bin/gendiff.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import { Command } from 'commander/esm.mjs';
+
+const program = new Command();
+
+program
+.description('Compares two configuration files and shows a difference.')
+.version('3.4.4')
+.option('-f, --format <type>', 'output format')
+.argument('<filepath1>')
+.argument('<filepath2>')
+.action((filepath1, filepath2) => console
+.log(genDiff(filepath1, filepath2, program.opts().format)))
+.parse();
