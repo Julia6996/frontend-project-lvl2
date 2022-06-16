c183496b34c6624d8cfaa174b1e0a85930d552bc
Julia6996 <ronin1991@rambler.ru>
2022-06-16 19:09:28 +0300

    test json file

--- Makefile
+++ Makefile
@@ -1,11 +1,22 @@
 install:
 	npm ci
+	npm link
 
 gendiff:
 	node bin/gendiff.js
 
 publish:
 	npm publish --dry-run
+
 lint:
 	npx eslint .	
 
+test: # Run tests
+	npm test
+
+test-watch: # Run tests with watch
+	npm test -s -- --watch
+
+test-coverage: # Run coverage tests
+	npm test -- --coverage --coverageProvider=v8
+	
---
+++ __tests__/test.js
@@ -0,0 +1,17 @@
+import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import fs from 'fs';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+
+test('gendiff file test json', () => {
+const expected = fs.readFileSync(getFixturePath('correct-json.txt'), 'utf-8');
+
+const result = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+expect(result).toEqual(expected);
+});
